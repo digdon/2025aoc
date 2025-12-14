f61959742387f59cb8618366848ab3c6d0fbf9a4
Mike Digdon <32070772+digdon@users.noreply.github.com>
2025-12-13 22:57:20 -0400

    Added a new merge mechanism, which is simpler and much faster

--- day_05/day05.go
+++ day_05/day05.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 )
@@ -50,7 +51,13 @@ func main() {
 		}
 	}
 
-	// Part 1 stuff
+	part1(ranges, idList)
+	part2(ranges)
+	part2Redux(ranges)
+}
+
+// Part 1 stuff
+func part1(ranges []Range, idList []int) {
 	begin := time.Now()
 
 	part1FreshCount := 0
@@ -65,9 +72,11 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d (%v)\n", part1FreshCount, time.Since(begin))
+}
 
-	// Part 2 stuff
-	begin = time.Now()
+// Part 2 stuff
+func part2(ranges []Range) {
+	begin := time.Now()
 
 	mergedRanges := []Range{}
 
@@ -76,14 +85,12 @@ func main() {
 
 		contained := false
 
-		for _, mr := range mergedRanges {
+		for i, mr := range mergedRanges {
 			if r.Start >= mr.Start && r.Start <= mr.End && r.End >= mr.Start && r.End <= mr.End {
-				// Fully-encompassed by merged range - split to make room
-				// newRanges = append(newRanges, Range{Start: mr.Start, End: r.Start - 1})
-				// newRanges = append(newRanges, Range{Start: r.End + 1, End: mr.End})
-				// Fully-encompassed by merged range - keep the merged range and ignore the new one
-				newRanges = append(newRanges, mr)
+				// Fully-encompassed by merged range - keep the rest of the merged ranges and ignore the new one
+				newRanges = append(newRanges, mergedRanges[i:]...)
 				contained = true
+				break
 			} else if r.Start <= mr.Start && r.End >= mr.End {
 				// Fully-encompasses merged range - ignore the merged range
 				continue
@@ -118,4 +125,44 @@ func main() {
 type Range struct {
 	Start int
 	End   int
+}
+
+// New method for merging ranges. We start by sorting the ranges, then comparing each range with
+// the last merged range, copying directly if there's no overlap, and joining them together if they overlap.
+// This is much simpler and far faster than the previous method.
+func part2Redux(ranges []Range) {
+	begin := time.Now()
+
+	sort.Slice(ranges, func(i, j int) bool {
+		if ranges[i].Start == ranges[j].Start {
+			return ranges[i].End < ranges[j].End
+		}
+
+		return ranges[i].Start < ranges[j].Start
+	})
+
+	mergedRanges := []Range{ranges[0]}
+
+	for i := 1; i < len(ranges); i++ {
+		first := mergedRanges[len(mergedRanges)-1]
+		second := ranges[i]
+
+		if first.End < second.Start {
+			// No overlap, add second to merged ranges
+			mergedRanges = append(mergedRanges, second)
+		} else {
+			// Overlap, merge the two ranges
+			newRange := Range{Start: min(first.Start, second.Start), End: max(first.End, second.End)}
+			mergedRanges[len(mergedRanges)-1] = newRange
+		}
+	}
+
+	// fmt.Println(mergedRanges)
+	part2FreshCount := 0
+
+	for _, r := range mergedRanges {
+		part2FreshCount += r.End - r.Start + 1
+	}
+
+	fmt.Printf("Part 2 Redux: %d (%v)\n", part2FreshCount, time.Since(begin))
 }
